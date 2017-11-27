a802f02a8fe395adc9022bd9fceafd0af4ec768c
DocLJN <ljnoble@gmail.com>
2017-11-27 19:59:23 +0000

    Create files for library extension project.

---
+++ library/library.rb
@@ -0,0 +1,16 @@
+#library.rb
+
+Model a Library as a class.
+
+* Create a class for a Library that has an array of books. Each book should be a hash with a title, which is a string, and rental details, which is another hash with a student name and due date.
+
+
+  {
+    title: "lord_of_the_rings",
+    rental_details: {
+     student_name: "Jeff",
+     date: "01/12/16"
+    }
+  }
+
+# end library.rb
---
+++ library/specs/library_spec.rb
@@ -0,0 +1,19 @@
+# library_spec.rb
+require "minitest/autorun"
+require "minitest/rg"
+require_relative ("../library")
+
+
+class TestTeam < Minitest::Test
+
+end
+
+
+
+  # * Create a method that list all the books and their corresponding details.
+  # * Create a method that takes in a book title and returns all of the information about that book.
+  # * Create a method that takes in a book title and returns only the rental details for that book.
+  # * Create a method that takes in a book title and adds it to our book list (Add a new hash for the book with the student name and date being left as empty strings)
+  # * Create a method that changes the rental details of a book by taking in the title of the book, the student renting it and the date it's due to be returned.
+
+# end library_spec.rb
