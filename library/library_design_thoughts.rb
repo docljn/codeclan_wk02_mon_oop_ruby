7548128a26e4ac73b256a8cb9c6ac2e82af00aa4
DocLJN <ljnoble@gmail.com>
2017-11-27 23:00:48 +0000

    List design thoughts for improving Library system.

---
+++ library/library_design_thoughts.rb
@@ -0,0 +1,19 @@
+# Wondering if this kind of structure would work better?
+
+
+# class Book
+#  title - read only
+#  borrower
+#  due date
+#  home library
+#  holds
+
+# class Borrower
+# name
+# max books checked out = number
+# number books checked out = number <= max
+
+# class Library
+# name
+# catalogue = all books with home library = name
+# book in stock = subset with no borrower listed
